a57defe8698c14a14540bb90dfaeac311dd9c4d5
Alex-dev1356 <asoliven26@yahoo.com>
2025-11-30 07:53:50 +0800

    Creation of our Models

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Restful_API.Models
+{
+    //Models serves as a blueprint or FORMAT for the data we are working with in our API
+    //This is the Format on how we are going to structure our data
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = null!; //We gave ut a Null value to avoid warnings   
+        public string Author { get; set; } = null!; //We gave ut a Null value to avoid warnings   
+        public int YearPubished { get; set; }
+    }
+}
